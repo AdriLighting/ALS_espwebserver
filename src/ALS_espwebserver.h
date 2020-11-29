6661054b110408dc1ce34dfb6d6c97b4b104f379
Grellard Adrien <grelard.adrien@gmail.com>
2020-11-29 02:49:48 +0100

    upd

---
+++ src/ALS_espwebserver.h
@@ -0,0 +1,25 @@
+#ifndef ALS_ESPWEBSERVER_H
+	#define ALS_ESPWEBSERVER_H
+	#include <Arduino.h>
+
+	class ALS_espwebserver
+	{
+	public:
+		ALS_espwebserver();
+		~ALS_espwebserver(){};
+		void setup();
+
+		void serverLoop();
+		void socketLoop();
+
+		void serverFS(boolean fsOK);
+		void serverInitialize();
+
+		void socketBegin();
+		void serverBegin();
+
+		void http_sendJson(String json);
+		
+	};		
+	ALS_espwebserver * ALS_espwebserverPtr_get();
+#endif
---
+++ src/ALS_webserver.cpp
@@ -0,0 +1,32 @@
+#include "ALS_espwebserver.h"
+#include <adri_espwebserver.h>
+
+extern adri_webserver	clientServer;
+extern adri_socket		socketServer;
+
+	ALS_espwebserver * ALS_espwebserverPtr;
+	ALS_espwebserver * ALS_espwebserverPtr_get(){return ALS_espwebserverPtr;}
+
+ALS_espwebserver::ALS_espwebserver(){
+	ALS_espwebserverPtr=this;
+	// webserver_ptr = this;
+ //    // clientServer = new AsyncWebServer(port);
+	// setup(fsOK, port);
+
+}
+
+void ALS_espwebserver::serverFS(boolean fsOK) {
+	clientServer.filesystem_ok(fsOK);
+	if (fsOK) clientServer.filesystem_set(&SPIFFS);
+}
+void ALS_espwebserver::serverInitialize()	{clientServer.initialize(80);	}
+
+void ALS_espwebserver::socketBegin() 		{socketServer.setup();}
+void ALS_espwebserver::serverBegin() 		{clientServer._server.begin();}
+
+void ALS_espwebserver::socketLoop()			{socketServer.loop();}
+void ALS_espwebserver::serverLoop() 		{clientServer.handleLoop();}
+
+void ALS_espwebserver::http_sendJson(String json){
+	clientServer.handleJson(json);
+}
